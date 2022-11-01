952bd016232888fba9812a1d2d07be51fe8efb07
anggapahlevi9@gmail.com <anggapahlevi09@gmail.com>
2022-11-01 19:04:17 +0700

    edit Index.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET/POST home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Kopy.Co Teams' });
 });
 
 module.exports = router;
