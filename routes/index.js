91d8ccc9bff9d38a9f33fc216870b5a33024aa68
anggapahlevi9@gmail.com <anggapahlevi09@gmail.com>
2022-11-16 19:48:52 +0700

    coba UTS input carsdata dan tes read csv file

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Kopy.Co Teams' });
 });
 
+/* Coba bikin route*/
+router.get('/contact', (req, res) => {
+    res.send('Hello World!')
+  })
+
 module.exports = router;
